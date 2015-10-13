3bdf6ad21d7414c09a627c2d1523672c013e0979
Germain Boue <boue.germain@gmail.com>
2015-10-12 17:39:48 -0700

    Add css for NewsHeader and fixed typo where missing '#' and was not rendering due to the fact it did not recognize DOM element

--- js/NewsHeaderTest.js
+++ js/NewsHeaderTest.js
@@ -2,4 +2,4 @@ var $ = require('jquery');
 var NewsHeader = require('./NewsHeader');
 var React = require('react');
 
-React.render(<NewsHeader />, $('content')[0]);
+React.render(<NewsHeader />, $('#content')[0]);
