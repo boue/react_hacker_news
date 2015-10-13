5362a2f3656a61fd4f6b3905068cd748f249d990
Germain Boue <boue.germain@gmail.com>
2015-10-12 20:10:45 -0700

    Fill in the nav bar with sections

--- js/NewsHeader.js
+++ js/NewsHeader.js
@@ -1,5 +1,6 @@
 var $ = require('jquery');
 var React = require('react');
+var _ = require('lodash');
 
 var NewsHeader = React.createClass({
   getLogo: function(){
@@ -18,14 +19,57 @@ var NewsHeader = React.createClass({
     );
   },
 
+  getNav: function(){
+    var navLinks = [
+      {
+        name: 'new',
+        url: 'newest'
+      },
+      {
+        name: 'comments',
+        url: 'newcomments'
+      },
+      {
+        name: 'show',
+        url: 'show'
+      },
+      {
+        name: 'ask',
+        url: 'ask'
+      },
+      {
+        name: 'jobs',
+        url: 'jobs'
+      },
+      {
+        name: 'submit',
+        url: 'submit'
+      }
+    ];
+
+    return (
+      <div className="newsHeader-nav">
+        {_(navLinks).map(function(navLink){
+          return(
+            <a key={navLink.url} className="newsHeader-navLink newsHeader-textLink" href={'https://news.ycombinator.com/' + navLink.url}>
+              {nav.Link.name}
+            </a>
+          );
+        }).value()}
+      </div>
+    );
+  },
+
   render: function(){
     return(
       <div className="newsHeader">
         {this.getLogo()}
         {this.getTitle()}
+        {this.getNav()}
       </div>
     );
   }
+
 });
 
 module.exports = NewsHeader;
